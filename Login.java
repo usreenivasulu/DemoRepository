5f5c5339348d88504915c85206b94dec0f471f8c
usreenivasulu <43570992+usreenivasulu@users.noreply.github.com>
2020-03-13 07:30:35 +0530

    added this validate()

--- Login.java
+++ Login.java
@@ -2,6 +2,11 @@ public class Login {
   
   private String username;
   private String password;
+  
+  //from develop2 branch added this validate()
+  public boolean validate(){
+  return true;
+  }
 
 public static void main(String[] args){
   System.out.println("From Main method");
